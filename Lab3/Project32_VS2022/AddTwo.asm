79714640bfd9b0697e84470e1b2ca7da58c2a836
Ahmed8881 <buttahmed2004@gmail.com>
2025-01-27 10:04:55 +0500

    added lab 3

--- Lab3/Project32_VS2022/AddTwo.asm
+++ Lab3/Project32_VS2022/AddTwo.asm
@@ -1,17 +1,43 @@
-; AddTwo.asm - adds two 32-bit integers.
-; Chapter 3 example
-
 .386
 .model flat,stdcall
 .stack 4096
-ExitProcess proto,dwExitCode:dword
+ExitProcess proto, dwExitCode:dword
+
+.data
+Rval SDWORD ?
+Xval SDWORD 26
+Yval SDWORD 30
+Zval SDWORD 40
 
 .code
-main proc
-	mov	ECX,5	
-	mov eax,1
-	add	eax,ecx				
+main PROC
+    ; Set and reset Zero Flag (ZF)
+    mov al, 1
+    add al, -1      ; ZF = 1
+    add al, 1       ; ZF = 0
 
-	invoke ExitProcess,0
-main endp
-end main
+    ; Set and reset Sign Flag (SF)
+    mov al, 0
+    add al, -1      ; SF = 1
+    add al, 1       ; SF = 0
+
+    ; Set and reset Carry Flag (CF)
+    mov al, +127
+    add al, 1   
+    add al, 1 
+
+    ; Set and reset Overflow Flag (OF)
+    mov al, +127
+    add al, 1       ; OF = 1
+    add al, 1      ; OF = 0
+        ; Set and reset Overflow Flag (OF)
+    mov al, 2
+    add al, 2      
+    add al, 1      
+
+  
+
+
+    INVOKE ExitProcess, 0
+main ENDP
+END main

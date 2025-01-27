6688804df9b7cf4c4b923fbb5a5bed601bc9cd12
Ahmed8881 <buttahmed2004@gmail.com>
2025-01-27 11:27:34 +0500

    completed lab 3

--- Lab3/Project32_VS2022/AddTwo.asm
+++ Lab3/Project32_VS2022/AddTwo.asm
@@ -1,17 +1,43 @@
-; AddTwo.asm - adds two 32-bit integers.
-; Chapter 3 example
-
+; SolveEquation.asm - solves the equation (10 - 7) * (5 + 6) * 9 using only the EAX register
+; Example program
+include irvine32.inc
 .386
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
+.data
+    result1 DWORD ?
+   
+
 
 .code
 main proc
-	mov	ECX,5	
-	mov eax,1
-	add	eax,ecx				
+ ;first step
+    mov EAX, 10
+    sub EAX, 7
+    mov result1, EAX
+    ;step 2
+    mov EAX, 5
+    add EAX, 6
 
-	invoke ExitProcess,0
+    ; Multiply the results of (10 - 7) and (5 + 6)
+    imul EAX, result1
+
+    ; Multiply the result by 9
+    imul EAX, 9
+
+ call WriteInt
+ ;first step
+ mov EAX ,5
+ imul EAX, 4
+
+ mov EBX,3
+ add EBX,7
+ imul EBX, 21
+ sub EAX, EBX
+ call WriteInt
+
+
+    invoke ExitProcess, 0
 main endp
 end main

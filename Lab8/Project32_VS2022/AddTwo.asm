0425ebaac7a6de362bbe37fd6704eab7f368d832
Ahmed8881 <buttahmed2004@gmail.com>
2025-03-24 10:22:20 +0500

    changes

--- Lab8/Project32_VS2022/AddTwo.asm
+++ Lab8/Project32_VS2022/AddTwo.asm
@@ -1,43 +1,62 @@
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto, dwExitCode:dword
+; filepath: d:\UET\COAL LAB\COAL\Lab8\Project32_VS2022\AddTwo.asm
+INCLUDE Irvine32.inc
 
 .data
-Rval SDWORD ?
-Xval SDWORD 26
-Yval SDWORD 30
-Zval SDWORD 40
+promptMsg db "Enter student marks (0-100): ", 0
+gradeA db "Grade: A", 0
+gradeB db "Grade: B", 0
+gradeC db "Grade: C", 0
+invalidMsg db "Invalid marks!", 0
+marksMsg db "Student marks: ", 0
 
 .code
 main PROC
-    ; Set and reset Zero Flag (ZF)
-    mov al, 1
-    add al, -1      ; ZF = 1
-    add al, 1       ; ZF = 0
-
-    ; Set and reset Sign Flag (SF)
-    mov al, 0
-    add al, -1      ; SF = 1
-    add al, 1       ; SF = 0
-
-    ; Set and reset Carry Flag (CF)
-    mov al, +127
-    add al, 1   
-    add al, 1 
-
-    ; Set and reset Overflow Flag (OF)
-    mov al, +127
-    add al, 1       ; OF = 1
-    add al, 1      ; OF = 0
-        ; Set and reset Overflow Flag (OF)
-    mov al, 2
-    add al, 2      
-    add al, 1      
-
-  
-
-
-    INVOKE ExitProcess, 0
+    mov edx, OFFSET promptMsg
+    call WriteString
+    call ReadInt
+    mov ebx, eax
+    
+    mov edx, OFFSET marksMsg
+    call WriteString
+    mov eax, ebx
+    call WriteDec
+    call Crlf
+    
+    cmp ebx, 0
+    jl invalid
+    cmp ebx, 100
+    jg invalid
+    
+    cmp ebx, 90
+    jg gradeIsA
+    
+    cmp ebx, 80
+    jg gradeIsB
+    
+    cmp ebx, 70
+    jg gradeIsC
+    
+    jmp invalid
+    
+gradeIsA:
+    mov edx, OFFSET gradeA
+    jmp displayGrade
+    
+gradeIsB:
+    mov edx, OFFSET gradeB
+    jmp displayGrade
+    
+gradeIsC:
+    mov edx, OFFSET gradeC
+    jmp displayGrade
+    
+invalid:
+    mov edx, OFFSET invalidMsg
+    
+displayGrade:
+    call WriteString
+    call Crlf
+    
+    exit
 main ENDP
 END main
